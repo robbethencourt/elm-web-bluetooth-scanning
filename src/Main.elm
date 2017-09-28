21375503caf92cca6ebdbb6874f9ebc40498c073
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-09-28 13:29:25 -0400

    added initial files and wing css template

---
+++ src/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, h1)
+import Html.Attributes exposing (class)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ h1 [ class "text-center" ] [ text "Elm Web Bluetooth Scanning App!" ] ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
