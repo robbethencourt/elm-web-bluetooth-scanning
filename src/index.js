6a4a4fb3a9a6bafcc1ed413e0edfa4eaaa61dfd0
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-09-28 20:05:22 -0400

    can now search for bluetooth devices in chrome experimental

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import './main.css'
 import { Main } from './Main.elm'
 
-Main.embed(document.getElementById('root'))
+const app = Main.embed(document.getElementById('root'))
+
+// Elm Subscriptions
+
+app.ports.requestBluetoothConnection.subscribe((testing) => {
+  console.log(testing)
+  navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+    .then(device => {
+      // Human-readable name of the device.
+      console.log(device.name)
+
+      // Attempts to connect to remote GATT Server.
+      return device.gatt.connect()
+    })
+    .then(server => {
+      /* ... */
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
