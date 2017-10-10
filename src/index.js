2dd55b2e4a88f462677e3ebebe357e4ef6f186ea
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-10-10 15:51:52 -0400

    bluetooth connection test on github pages

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ const app = Main.embed(document.getElementById('root'))
 
 // Elm Subscriptions
 
-app.ports.requestBluetoothConnection.subscribe((testing) => {
-  console.log(testing)
+app.ports.requestBluetoothConnection.subscribe(() => {
   navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-    .then(device => {
-      // Human-readable name of the device.
-      console.log(device.name)
-
-      // Attempts to connect to remote GATT Server.
-      return device.gatt.connect()
-    })
+    .then(device => device.gatt.connect())
     .then(server => {
-      /* ... */
+      // Getting Battery Service...
+      return server.getPrimaryService('battery_service')
+    })
+    .then(service => {
+      // Getting Battery Level Characteristic...
+      return service.getCharacteristic('battery_level')
+    })
+    .then(characteristic => {
+      // Reading Battery Level...
+      return characteristic.readValue()
+    })
+    .then(value => {
+      console.log('Battery percentage is ' + value.getUint8(0))
     })
     .catch(error => {
       console.log(error)
