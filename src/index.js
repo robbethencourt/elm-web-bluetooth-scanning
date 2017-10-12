1c05724f1f74babc79d6ceb0e5bcb13f9f234a45
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-10-12 13:24:21 -0400

    build

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = Main.embed(document.getElementById('root'))
 // Elm Subscriptions
 
 app.ports.requestBluetoothConnection.subscribe(() => {
-  navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+  navigator.bluetooth.requestDevice({ filters: [{ services: ['0000180f-0000-1000-8000-00805f9b34fb'] }] })
     .then(device => {
       console.log(device)
       app.ports.bluetoothConnectionResponse.send(device.name)
@@ -14,22 +14,27 @@ app.ports.requestBluetoothConnection.subscribe(() => {
     })
     .then(server => {
       // Getting Battery Service...
-      console.log(server)
-      console.log(server.getPrimaryServices())
-      return server.getPrimaryService('battery_service')
+      return server.getPrimaryService('0000180f-0000-1000-8000-00805f9b34fb')
     })
     .then(service => {
+      console.log(service)
       // Getting Battery Level Characteristic...
-      return service.getCharacteristic('battery_level')
+      return service.getCharacteristic('00002a19-0000-1000-8000-00805f934fb')
     })
     .then(characteristic => {
+      console.log(characteristic)
       // Reading Battery Level...
       return characteristic.readValue()
     })
     .then(value => {
+      console.log(value)
       console.log('Battery percentage is ' + value.getUint8(0))
     })
     .catch(error => {
       console.log(error)
     })
 })
+
+// service:         0000180f-0000-1000-8000-00805f9b34fb
+// characteristic:  00002a19-0000-1000-8000-00805f9b34fb
+// descriptors:     00002904-0000-1000-8000-00805f934fb / 00002908-0000-1000-8000-00805f934fb / 00002902-0000-1000-8000-00805f934fb
